9ee811e7c75168f5af5878cfaeb19aa1616c678a
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-06-17 22:06:21 +0530

    Create qs4.java

---
+++ QS_1/qs4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            digits[i] += 1;
+
+            if (digits[i] == 10) {
+                digits[i] = 0;
+            } else {
+                return digits;
+            }
+        }
+
+        int[] result = new int[n + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+
+        Solution solution = new Solution();
+        int[] result = solution.plusOne(digits);
+
+        System.out.print("Result: ");
+        for (int digit : result) {
+            System.out.print(digit + " ");
+        }
+    }
+}
