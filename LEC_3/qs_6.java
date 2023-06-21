7db8fac868f667b543820db013d937a2b5a623fd
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-06-21 20:32:01 +0530

    Create qs_6.java

---
+++ LEC_3/qs_6.java
@@ -0,0 +1,11 @@
+public class qs_6 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        
+        for (int num : nums) {
+            result ^= num;
+        }
+        
+        return result;
+    }
+}
